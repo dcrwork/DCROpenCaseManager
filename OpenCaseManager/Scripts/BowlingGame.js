d6bae1ff25624c8a66703f23fc3ae14c485e6181
Daniel Guldberg Aaes <guldberg72@gmail.com>
2019-03-20 15:49:01 +0100

    ..

---
+++ OpenCaseManager/Scripts/BowlingGame.js
@@ -0,0 +1,13 @@
+﻿// BowlingGame.js
+(function (window) {
+    function roll(pins) {
+        return 0;
+    };
+
+    function score() {
+        return 0;
+    };
+
+    window.roll = roll;
+    window.score = score;
+})(window);
