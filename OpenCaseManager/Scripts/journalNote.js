ed8622219b0d740a4e5d301419ce111828c20061
Allan Kjær Nielsen <allankrn2@gmail.com>
2019-03-27 14:37:37 +0100

    update date modal

--- OpenCaseManager/Scripts/journalNote.js
+++ OpenCaseManager/Scripts/journalNote.js
@@ -2,6 +2,19 @@
     var myWindow = window.open("/JournalNote/Create", "", "width=1200,height=1200");
 }
 
+function formatDate(date) {
+    var value = new Date(date);
+    return `${value.getDate()}/${value.getMonth()}/${value.getFullYear()}`;
+}
+
+function changedate(inputId, lableId) {
+    var value = $('#' + inputId).val();
+    var applyTo = $('#' + lableId)[0];
+    applyTo.value = formatDate(value).toString();
+    applyTo.textContent = formatDate(value).toString();
+}
+
+
 if (window.location.pathname === "/journalnote/create") {
     var header = $("header")[0];
     header.style.display = 'none';
