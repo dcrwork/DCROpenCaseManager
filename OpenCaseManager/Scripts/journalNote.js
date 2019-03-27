03dbc52fd6eca2df20f43e18aea9f0dd8ebb2c6d
Allan Kjær Nielsen <allankrn2@gmail.com>
2019-03-27 10:14:19 +0100

    Remove header from create journal note popup.

--- OpenCaseManager/Scripts/journalNote.js
+++ OpenCaseManager/Scripts/journalNote.js
@@ -1,3 +1,8 @@
 ﻿function CreateJournalNoteView() {
     var myWindow = window.open("/JournalNote/Create", "", "width=1200,height=1200");
-} 
+}
+
+if (window.location.pathname === "/journalnote/create") {
+    var header = $("header")[0];
+    header.style.display = 'none';
+}
