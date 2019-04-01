ec337b0a0cbd6a1180ffbbfae15d96f2e4fc8d0a
Allan Kjær Nielsen <allankrn2@gmail.com>
2019-04-01 15:12:30 +0200

    Change datepicker

--- OpenCaseManager/Scripts/journalNote.js
+++ OpenCaseManager/Scripts/journalNote.js
@@ -1,21 +1,22 @@
 ﻿function CreateJournalNoteView() {
-    var myWindow = window.open("/JournalNote/Create", "", "width=1200,height=1200");
+    window.open("/JournalNote/Create", "", "width=1200,height=1200");
 }
 
 function formatDate(date) {
     var value = new Date(date);
-    return value.getDate() + "/" + value.getMonth() + "/" + value.getFullYear();
+    console.log(value);
+    return value.getDate() + "/" + (value.getMonth()+1) + "/" + value.getFullYear();
 }
+
+$(function () {
+    $("#datepicker").datepicker();
+    $("#datepicker").datepicker("option", "dateFormat", "d/m/yy");
+});
 
 function changedate(inputId, lableId) {
     var value = $('#' + inputId).val();
     var applyTo = $('#' + lableId)[0];
-    applyTo.value = formatDate(value).toString();
-    applyTo.textContent = formatDate(value).toString();
+    applyTo.value = value;
+    applyTo.textContent = value;
 }
 
-
-if (window.location.pathname === "/journalnote/create") {
-    var header = $("header")[0];
-    header.style.display = 'none';
-}
