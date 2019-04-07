bdf54cc41d4168f6340826b8712d0d9689632d81
Thais Kure Corneliussen <thco@itu.dk>
2019-04-07 16:14:02 +0200

    Data from db retreived for child page

--- OpenCaseManager/Scripts/child.js
+++ OpenCaseManager/Scripts/child.js
@@ -1,3 +1,32 @@
 ﻿$(document).ready(function () {
+    var childId = App.getParameterByName("id", window.location.href);
+    var query = {
+        "type": "SELECT",
+        "entity": "ChildInstances",
+        "resultSet": ["*"],
+        "filters": new Array(),
+        "order": []
+    }
 
+    var whereChildIdMatchesFilter = {
+        "column": "ChildId",
+        "operator": "equal",
+        "value": childId,
+        "valueType": "int",
+        "logicalOperator": "and"
+    };
+    query.filters.push(whereChildIdMatchesFilter);
+
+    API.service('records', query)
+        .done(function (response) {
+            showChildInstances(response);
+        })
+        .fail(function (e) {
+            reject(e);
+        });
+});
+
+function showChildInstances(response) {
+    var result = JSON.parse(response);
+    console.log(result);
 }
