97eea0c1df5d3a1e016b70d974299141b4bdcc23
Thais Kure Corneliussen <thco@itu.dk>
2019-04-07 15:36:44 +0200

    Added a database query

---
+++ OpenCaseManager.Database/dbo/Schema_Modifications/Views/ChildInstances.sql
@@ -0,0 +1,9 @@
+CREATE VIEW dbo.ChildInstances AS
+SELECT I.Id, I.Title, U.Name, I.NextDeadline, I.IsOpen, I.Description, P.Title AS Process, C.Id AS ChildId
+FROM dbo.Instance AS I, dbo.InstanceExtension AS IE, dbo.Child AS C, dbo.[User] AS U, dbo.Process AS P
+WHERE I.Id = IE.InstanceId
+	AND IE.ChildId = C.Id
+	AND I.Responsible = U.Id
+	AND I.GraphId = P.GraphId
+GO
+GO
