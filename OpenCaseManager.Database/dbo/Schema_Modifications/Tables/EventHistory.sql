0f31a6ba50047ff2e38dae9ddd18df6bb81559cf
jmog <jmog@itu.dk>
2019-04-08 10:07:58 +0200

    Updated sql files and added some Jasmine files for deployment reasons

--- OpenCaseManager.Database/dbo/Schema_Modifications/Tables/EventHistory.sql
+++
@@ -1,2 +0,0 @@
-﻿ALTER TABLE EventHistory
-	ADD ExecutionDate Datetime;
--- OpenCaseManager.Database/dbo/Schema_Modifications/Tables/InstanceExtension.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE InstanceExtension
-	ADD ChildId INT;
-
-ALTER TABLE InstanceExtension
-	ADD FOREIGN KEY (ChildId) REFERENCES Child(Id);
