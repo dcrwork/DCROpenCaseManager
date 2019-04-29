494ea5bf37044b9db76f01ad0aac422bf2f038f9
Daniel Guldberg Aaes <guldberg72@gmail.com>
2019-04-29 12:22:37 +0200

    ....

--- OpenCaseManager/Scripts/home.js
+++ OpenCaseManager/Scripts/home.js
@@ -1,51 +1,10 @@
 ﻿
 $(document).ready(function () {
-    $('#createInstance').on('click', function (e) {
-
-        var title = $('#instanceTitle').val();
-        var graphId = $('#instanceProcesses').find(":selected").val();
-
-        var userRoles = new Array()
-        $('select[name="multi-select"]').each(function (index, select) {
-            var userRole = { roleId: $(select).attr('userid'), userId: '' }
-            $(select.selectedOptions).each(function (index, item) {
-                if (item.value !== '0')
-                    userRole.userId = item.value;
-            })
-            if (userRole.userId !== '')
-                userRoles.push(userRole)
-        })
-
-        if (title !== '' && graphId > 0) {
-            App.addInstance(title, graphId, userRoles);
-            App.getMyInstances();
-            $('#addNewInstance').modal('toggle');
-        }
-        else {
-            App.showWarningMessage(translations.InstanceCreateError);
-        }
-        e.preventDefault();
-    });
-
-    var promise = new Promise(function (resolve, reject) {
-        App.responsible(resolve);
-    });
-
-    promise.then(function (result) {
-        App.getProcess(true, true);
-        App.getMyInstances();
-        App.getTasks();
-    }, function (err) {
-        console.log(err); // Error: "It broke"
-    });
-
-    $('#instanceProcesses').on('change', function () {
-        var graphId = $('#instanceProcesses').find(":selected").val();
-        App.getRoles(graphId);
-    });
+    getDataForAllMyChildren();
 });
+ 
 
-$(document).ready(function () {
+function getDataForAllMyChildren() {
 
     query = {
         "type": "SELECT",
@@ -76,8 +35,7 @@ $(document).ready(function () {
         }
         e.preventDefault();
     });
-});
- 
+}
 
 function displayChildren(response) {
     var result = JSON.parse(response);
@@ -102,6 +60,7 @@ function addZero(i) {
 function getChildInstanceHtml(item) {
     var childLink = "../Child?id=" + item.ChildId;
     var objDate = new Date(item.NextDeadline);
+    var objTest = new Date("2019-04-17 09:43:24.5334795");
     var date = objDate.getFullYear() + "-" + addZero((objDate.getMonth() + 1)) + "-" + addZero(objDate.getDay());
     var time = addZero(objDate.getHours()) + ":" + addZero(objDate.getMinutes());
     var returnHtml = "<tr class='trStyleClass'>";
@@ -110,7 +69,7 @@ function getChildInstanceHtml(item) {
     returnHtml += "<td><a href='" + childLink + "'>" + item.ChildName + "</a></td>";
     returnHtml += "<td>123456-7890</td>";
     returnHtml += "<td>" + item.Responsible + "</td>";
-    returnHtml += (item.NextDeadline != null) ? "<td >" + date + " " + time + "</td>" : "<td>Ingen kommende deadlines</td>";
+    returnHtml += (item.NextDeadline != null) ? "<td >" + date + " " + time + "</td>" : "<td>ngen kommende deadlines</td>";
 
     returnHtml += "</tr>";
     return returnHtml;
@@ -127,13 +86,9 @@ function getStatus(item) {
         } else if (now + 604800000 >= deadline) {
             return "<span class='dot dotYellow'></span>";
         }
-
-        if (deadline == null && sumOfEvents > 0) {
-            return "<span class='dot dotGreen'></span>";
-        }
-    } 
-    
-    return "<span class='dot dotGreen'></span>";
+    } else if (sumOfEvents > 0) {
+        return "<span class='dot dotGreen'></span>";
+    }
 }
 
 
