2c17f89ef1d4e3989457366c2ba85e51c64092aa
Oliver Scharling Pedersen <olpe@itu.dk>
2019-05-03 13:03:06 +0200

    Added content to JournalHistoryForASingleInstance

---
+++ OpenCaseManager.Database/dbo/Functions/JournalHistoryForASingleInstance.sql
@@ -0,0 +1,39 @@
+/****** Object:  UserDefinedFunction [dbo].[JournalHistoryForASingleInstance]    Script Date: 03-05-2019 13:01:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		JoeJoe and T-Boi
+-- Create date: 27-03-2019
+-- Description:	Get history of past executed tasks for an instance
+-- =============================================
+CREATE FUNCTION [dbo].[JournalHistoryForASingleInstance](@InstanceId INT)
+RETURNS TABLE
+AS
+	RETURN (
+	           SELECT [EventDate],
+					  [Event].[EventId],
+					  [Event].[Id],
+	                  [Event].[Title],
+	                  [Responsible],
+					  [User].[Name] AS "ResponsibleName",
+	                  [Due],
+	                  [Description],
+	                  [IsEnabled],
+	                  [IsPending],
+	                  [IsIncluded],
+	                  [IsExecuted],
+	                  [EventType],
+	                  [Event].[InstanceId],
+	                  [JournalHistory].[Type]
+	           FROM   [dbo].[Event], [dbo].[JournalHistory], [dbo].[User]
+	           WHERE  [dbo].[Event].[Id] = [dbo].[JournalHistory].[EventId]
+					  AND [dbo].[Event].[InstanceId] = [dbo].[JournalHistory].[InstanceId]
+					  AND [dbo].[User].[Id] = [dbo].[Event].[Responsible]
+					  AND [dbo].[JournalHistory].[InstanceId]=@InstanceId
+	       )
+GO
+
